588d9734970fd5c3ce2abc9094f019cd55c722ee
Jonathan Chan <jchchan91@gmail.com>
2018-01-21 19:17:39 -0800

    Finish data cleaning functions

--- train_hotdog.py
+++ train_hotdog.py
@@ -1,5 +1,14 @@
+import logging
 from utils import data_set
 
 
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+
 if __name__ == '__main__':
-    data_set.open_data_set('hotdog_data')
+    data_path = 'C:/data/hotdog_data'
+    data_set.open_data_set(
+        path=data_path,
+        refresh_data=False)
+    data_set.clean_data(data_path)
