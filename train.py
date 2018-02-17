14ca0ee80c1820b5b305980cc2a26de7c2f4f8bc
Jonathan Chan <jchchan91@gmail.com>
2018-02-17 01:29:18 -0800

    Implement augmenting images

--- train.py
+++ train.py
@@ -4,6 +4,7 @@ import logging
 from sklearn.model_selection import train_test_split
 import numpy as np
 from PIL import Image
+from PIL import ImageFilter
 from keras.utils.np_utils import to_categorical
 import hotdog.models
 
@@ -12,7 +13,7 @@ logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 
-def load_image(img_path, img_size):
+def generate_augmented_image(img):
     # img = cv2.imread(img_path)
     # angle = np.random.randint(0, 360)
     # img = rotateImage(img, angle)
@@ -20,13 +21,28 @@ def load_image(img_path, img_size):
     # img = cv2.resize(img, img_size)
     # return img
 
-    img = Image.open(img_path)
+    # blur the image
+    if img.mode != 'RGB' and img.mode != 'L':
+        img = img.convert('L')
+    img = img.filter(ImageFilter.GaussianBlur(3))
+
+    # random rotation
+    angle = np.random.randint(0, 360)
+    img = img.rotate(angle)
+    
+    return img
+
+
+def convert_pil_image_to_nparray(
+        img,
+        img_size
+):
     img = img.resize(img_size, resample=Image.BILINEAR)
     img = img.convert('L')  # Convert RGB -> gray
     return np.expand_dims(np.array(img), axis=2)  # Make the shape -> (sz, sz, 1)
+    
 
-
-def load_img_class(class_paths, class_label, class_size, img_size):
+def load_image_class(class_paths, class_label, class_size, img_size):
     '''
     Loads all the image paths in class_paths into memory, and assign
     'class_label' to that image.
@@ -40,14 +56,17 @@ def load_img_class(class_paths, class_label, class_size, img_size):
     y = []
 
     for img_path in class_paths:
-        img = load_image(img_path, img_size)
-        x.append(img)
+        img_pil = Image.open(img_path)
+        img_np = convert_pil_image_to_nparray(img_pil, img_size)
+        x.append(img_np)
         y.append(class_label)
 
     while len(x) < class_size:
         randIdx = np.random.randint(0, len(class_paths))
-        img = load_image(class_paths[randIdx], img_size)
-        x.append(img)
+        img_pil = Image.open(class_paths[randIdx])
+        img_pil = generate_augmented_image(img_pil)
+        img_np = convert_pil_image_to_nparray(img_pil, img_size)
+        x.append(img_np)
         y.append(class_label)
 
     return x, y
@@ -74,8 +93,8 @@ def load_data(
     notHotdogs = glob.glob(nonhotdogs_path_pattern, recursive=True)
 
     img_size_2d = (img_size, img_size)
-    xHotdog, yHotdog = load_img_class(hotdogs, 0, class_size, img_size_2d)
-    xNotHotdog, yNotHotdog = load_img_class(notHotdogs, 1, class_size, img_size_2d)
+    xHotdog, yHotdog = load_image_class(hotdogs, 0, class_size, img_size_2d)
+    xNotHotdog, yNotHotdog = load_image_class(notHotdogs, 1, class_size, img_size_2d)
     print("There are", len(xHotdog), "hotdog images")
     print("There are", len(xNotHotdog), "not hotdog images")
 
